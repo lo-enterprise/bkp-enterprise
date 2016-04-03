5eaaea13f56af52aa19898712ae0a12f0b2ff6b7
Temp <lo-enterprise@users.noreply.github.com>
2016-04-03 12:00:24 +0200

    Initiate result model and back to standard async node js

--- server/boot/coin-change.js
+++ server/boot/coin-change.js
@@ -1,15 +1,75 @@
+var _ = require('lodash');
+var bkp = require('bkp');
 
 module.exports = function(app) {
     var CoinChange = app.models.CoinChange;
+    var Coin = app.models.Coin;
 
-    var connection = require('strong-mq')
-        .create('native:')
-        .open();
+    CoinChange.beforeCreate = function(next) {
+        var amount = this.amount;
+        var coinChange = this;
 
-    var push = connection.createPushQueue('bkp.Bounded');
+        function coinToItem(coin) {
+            return {
+                name: {
+                    id: coin.id,
+                    amount: coin.amount
+                },
+                weight: coin.amount,
+                value: coin.amount,
+                pieces: coin.quantity
+            };
+        }
 
-    CoinChange.afterCreate = function(next) {
-        push.publish(this);
-        next();
-    };
+        function itemToCoin(item) {
+            return {
+                amount: item.item.amount,
+                quantity: item.count,
+                id: item.item.id
+            }
+        }
+
+        function resultToCoinResult(result) {
+            return {
+                total: result.totalWeight,
+                coins: _.map(result.items, itemToCoin)
+            };
+        }
+
+        function mergeInCoinChange(coinResult) {
+            coinChange.result = coinResult;
+            return coinChange;
+        }
+
+        function substract(quantity) {
+            return function(coin) {
+                coin.quantity -= quantity;
+                return coin;
+            };
+        }
+
+        Coin.find()
+            .then(function(coins) {
+                var items = _.map(coins, coinToItem);
+                return bkp.Bounded(items, amount);
+            })
+            .then(resultToCoinResult)
+            .then(mergeInCoinChange)
+            .then(function(coinChange) {
+                var promises = _.map(coinChange.result.coins, function(coinResult) {
+                    return Coin
+                        .findById(coinResult.id)
+                        .then(substract(coinResult.quantity))
+                        .then(function (coin) {
+                            return Coin.upsert(coin);
+                        });
+                });
+                return Promise.all(promises);
+            })
+            .then(function (obj) {
+                console.log(obj);
+                next();
+            })
+            .catch(next);
+      };
 };
